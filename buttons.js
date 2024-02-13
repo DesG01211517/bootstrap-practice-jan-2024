023b389839db8d7cb59fbb11439d572c4e25917b
DesG01211517 <desmongarrison2@gmail.com>
2024-02-12 22:59:59 -0500

    done

--- buttons.js
+++ buttons.js
@@ -1,7 +1,15 @@
-LetsGo.onclick = function () {
-    alert("Get up and Move");
-    
+Button1.onclick = function () {
+    alert("It Works");
 }
 Button2.onclick = function () {
-    alert("Go Home");
+    alert("It Works");
 }   
+Button3.onclick = function () {
+    alert("It works");
+}
+Button4.onclick = function () {
+    alert("It Works");
+}
+Button5.onclick = function () {
+    alert("It Works");
+}
